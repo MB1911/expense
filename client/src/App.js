765809abd8a13321d93ff97238fa4735d85a8e7e
MB1911 <buch.manan2010@gmail.com>
2022-03-02 18:45:05 +0530

    api try

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState} from 'react';
 function App() {
+
+  const[data,setData]=useState("");
+  useEffect(()=>{
+    const req = await fetch("/demo");
+    const res = await req.text();
+    setData(res);
+  },[])
   return (
     <div className="App">
       Welcome to expense tracker!!!!
+      {data && <p>{data}</p>}
     </div>
   );
 }
--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require('express'); //Line 1
 const path = require("path");
 const app = express(); //Line 2
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+
 
 
 app.get("/",function(req,res){
 	res.send("ok");
 })
 
-
-app.get("*", (req, res) => {
+if(process.env.NODE_ENV === "production")
+{
+	app.use(express.static(path.join(__dirname, "client", "build")));	
+	app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+	});	
+}
+
+app.get("/demo",(req,res)=>{
+	res.send("demo");
+})
+
 const port = process.env.PORT || 5000; //Line 3	
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
