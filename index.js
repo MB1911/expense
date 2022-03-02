cfda567f2f88fdd08764aef5d76e0ef9e3c1c011
MB1911 <buch.manan2010@gmail.com>
2022-03-02 18:34:57 +0530

    new msg

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express'); //Line 1
+const path = require("path");
 const app = express(); //Line 2
 
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 
 app.get("/",function(req,res){
 	res.send("ok");
 })
 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 const port = process.env.PORT || 5000; //Line 3	
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
